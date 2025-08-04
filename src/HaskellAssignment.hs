3a96eec99ac3670b2aa9f1fa25176d1a1be03703
Alex Zimmer <164894355+AlexZimmzer@users.noreply.github.com>
2025-08-04 13:58:17 -0400

    Update HaskellAssignment.hs

--- src/HaskellAssignment.hs
+++ src/HaskellAssignment.hs
@@ -8,7 +8,12 @@ instance Show Found where
   show (Match index) = "Found match at " ++ show index
   show NoMatch = "No match found!"
 findFirst :: Eq a => (a -> Bool) -> [a] -> Found
-
+findFirst needle haystack = go 0 haystack
+  where
+    go [] _  = NoMatch -- if not found, return NoMatch
+    go i (x:xs) -- steps through, keeping track of i
+      | needle x = Match i -- if found at index i
+      | otherwise = go (i+1) xs -- keep going
 ------------------------------------------------
 -- palindrome
 ------------------------------------------------
