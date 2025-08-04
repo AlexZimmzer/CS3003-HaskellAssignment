07cb5f6cf69f50fe705e6deb21dd81ea4ee76080
Alex Zimmer <164894355+AlexZimmzer@users.noreply.github.com>
2025-08-04 14:02:02 -0400

    Update HaskellAssignment.hs

--- src/HaskellAssignment.hs
+++ src/HaskellAssignment.hs
@@ -14,8 +14,13 @@ findFirst needle haystack = go 0 haystack
     go i (x:xs) -- steps through, keeping track of i
       | needle x = Match i -- if found at index i
       | otherwise = go (i+1) xs -- keep going
+      
 ------------------------------------------------
 -- palindrome
 ------------------------------------------------
 palindrome :: [Char] -> Bool
 palindrome candidate = candidate == reverse candidate
+
+------------------------------------------------
+-- mergesort
+------------------------------------------------
