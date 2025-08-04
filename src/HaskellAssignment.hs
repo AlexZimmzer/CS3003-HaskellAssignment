3ad02e5f4b0ed8a375d874b3258bffc80326197f
Alex Zimmer <164894355+AlexZimmzer@users.noreply.github.com>
2025-08-04 14:24:33 -0400

    Update HaskellAssignment.hs

--- src/HaskellAssignment.hs
+++ src/HaskellAssignment.hs
@@ -24,3 +24,32 @@ palindrome candidate = candidate == reverse candidate
 ------------------------------------------------
 -- mergesort
 ------------------------------------------------
+mergesort :: (a -> a -> Bool) -> [a] -> [a]
+mergesort _ []  = []
+mergesort _ [x] = [x]
+mergesort cmp xs =
+  let (left, right) = splitAt (length xs `div` 2) xs
+  in merge cmp (mergesort cmp left) (mergesort cmp right)
+
+-- merge helper
+merge :: (a -> a -> Bool) -> [a] -> [a] -> [a]
+merge _ xs [] = xs
+merge _ [] ys = ys
+merge cmp (x:xs) (y:ys)
+  | cmp x y   = x : merge cmp xs (y:ys)
+  | otherwise = y : merge cmp (x:xs) ys
+
+------------------------------------------------
+-- runLengthEncode
+------------------------------------------------
+data RunLength = RunLength Int Char
+  deriving (Show, Eq)
+
+runLengthEncode :: String -> [RunLength]
+runLengthEncode [] = []
+runLengthEncode (x:xs) = encode 1 x xs
+  where
+    encode n c [] = [RunLength n c]
+    encode n c (y:ys)
+      | y == c    = encode (n + 1) c ys
+      | otherwise = RunLength n c : encode 1 y ys
